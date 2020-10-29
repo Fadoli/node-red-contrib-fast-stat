a9cc01842e2efed37a9c2b5afecfd4295399fc04
Franck <f13@laposte.net>
2020-10-29 21:32:43 +0100

    trash test deps for zora

--- test/statsArray.test.js
+++ test/statsArray.test.js
@@ -1,10 +1,9 @@
-require('tap').mochaGlobals()
-const should = require('should')
+const test = require("zora").test;
 const StatsArray = require("../src/statsArray");
 
 const TEST_PRECISION = 1000000000; // 10^-9 precision (9 can fail on sum/avg)
 
-function applyPrecision ( obj, precision = TEST_PRECISION ) {
+function applyPrecision(obj, precision = TEST_PRECISION) {
     const keys = Object.keys(obj);
     keys.forEach((key) => {
         obj[key] = Math.round(obj[key] * precision) / precision;
@@ -13,22 +12,22 @@ function applyPrecision ( obj, precision = TEST_PRECISION ) {
 }
 
 
-describe("statsArray", () => {
-    it('should not have stats when empty', function () {
+test("statsArray", (t) => {
+    t.test('should not have stats when empty', function () {
         const stat = new StatsArray(10);
-        should(stat.getStats()).eql(null);
+        t.deepEqual(stat.getStats(), null);
     });
 
-    it('Compute data on the go', function () {
+    t.test('Compute data on the go', function () {
         const stat = new StatsArray(10);
         stat.append(1);
-        stat.getStats().should.eql({ n: 1, min: 1, max: 1, sum: 1, mean: 1, variance: 0, standard_deviation: 0 });
+        t.deepEqual(stat.getStats(), { n: 1, min: 1, max: 1, sum: 1, mean: 1, variance: 0, standard_deviation: 0 });
         stat.append(-1);
-        stat.getStats().should.eql({ n: 2, min: -1, max: 1, sum: 0, mean: 0, variance: 1, standard_deviation: 1 });
+        t.deepEqual(stat.getStats(), { n: 2, min: -1, max: 1, sum: 0, mean: 0, variance: 1, standard_deviation: 1 });
         stat.append(1);
-        stat.getStats().should.eql({ n: 3, min: -1, max: 1, sum: 1, mean: 1 / 3, variance: 0.888888888888889, standard_deviation: 0.9428090415820634 });
+        t.deepEqual(stat.getStats(), { n: 3, min: -1, max: 1, sum: 1, mean: 1 / 3, variance: 0.888888888888889, standard_deviation: 0.9428090415820634 });
         stat.append(-1);
-        stat.getStats().should.eql({ n: 4, min: -1, max: 1, sum: 0, mean: 0, variance: 1, standard_deviation: 1 });
+        t.deepEqual(stat.getStats(), { n: 4, min: -1, max: 1, sum: 0, mean: 0, variance: 1, standard_deviation: 1 });
 
         stat.append(1);
         stat.append(-1);
@@ -37,10 +36,10 @@ describe("statsArray", () => {
         stat.append(1);
         stat.append(-1);
         // Final stats
-        stat.getStats().should.eql({ n: 10, min: -1, max: 1, sum: 0, mean: 0, variance: 1, standard_deviation: 1 });
+        t.deepEqual(stat.getStats(), { n: 10, min: -1, max: 1, sum: 0, mean: 0, variance: 1, standard_deviation: 1 });
     });
 
-    it('Compute data on the go : and correctly (no over-write)', function () {
+    t.test('Compute data on the go : and correctly (no over-wrt)', function () {
         const array = [];
         const SIZE = 42;
         let qt = SIZE;
@@ -55,15 +54,15 @@ describe("statsArray", () => {
         });
 
         // Important note :
-        // It is possible that due to how it is computed (incrementaly) variance changes a tad
-        // This means we have to reduce the number of digit that matters
+        // t.test is possible that due to how t.test is computed (incrementaly) variance changes a tad
+        // This means we have to reduce the number of digt.test that matters
         const res1 = applyPrecision(stat1.getStats());
         const res2 = applyPrecision(stat1.compute());
-        res1.should.eql(res2);
+        t.deepEqual(res1, res2);
     });
 
 
-    it('Compute data on the go : and correctly (with over-write)', function () {
+    t.test('Compute data on the go : and correctly (with over-wrt)', function () {
         const array = [];
         const SIZE = 42;
         let qt = SIZE;
@@ -83,14 +82,14 @@ describe("statsArray", () => {
         });
 
         // Important note :
-        // It is possible that due to how it is computed (incrementaly) variance changes a tad
-        // This means we have to reduce the number of digit that matters
+        // t.test is possible that due to how t.test is computed (incrementaly) variance changes a tad
+        // This means we have to reduce the number of digt.test that matters
         const res1 = applyPrecision(stat1.getStats());
         const res2 = applyPrecision(stat1.compute());
-        res1.should.eql(res2);
+        t.deepEqual(res1, res2);
 
-        res1.mean.should.eql(0);
-        res1.min.should.eql(0);
-        res1.variance.should.eql(0);
+        t.equal(res1.mean, 0);
+        t.equal(res1.min, 0);
+        t.equal(res1.variance, 0);
     });
 });
